748d6f3dbbdb4bf3409814acd9b86dc6fff1a80f
Tareq <tareqnmd@gmail.com>
2025-09-12 12:10:45 +0600

    Add authentication functionality: implement AuthController and AuthService with sign-in and sign-up methods, and update package.json with class-transformer and class-validator dependencies.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import { AuthService } from './providers/auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-in')
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+}
--- src/modules/auth/providers/auth.service.ts
+++ src/modules/auth/providers/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { SignInDto } from '../dto/sign-in.dto';
+import { SignUpDto } from '../dto/sign-up.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signIn(signInDto: SignInDto) {
+    return signInDto;
+  }
+
+  signUp(signUpDto: SignUpDto) {
+    return signUpDto;
+  }
+}
