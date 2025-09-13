3f93d9892e86aedad351296133b804c1df7bd974
Tareq <tareqnmd@gmail.com>
2025-09-13 13:21:57 +0600

    Refactor AuthModule providers: remove HassingProvider and streamline provider registration by using BcryptProvider for HashingProvider, enhancing password handling consistency.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './providers/auth.service';
+import { BcryptProvider } from './providers/bcrypt.provider';
+import { HashingProvider } from './providers/hashing.provider';
 import { SignInProvider } from './providers/sign-in.provider';
 import { SignUpProvider } from './providers/sign-up.provider';
 import { SocialProvider } from './providers/social.provider';
-import { HassingProvider } from './providers/hassing.provider';
-import { BcryptProvider } from './providers/bcrypt.provider';
-import { HashingProvider } from './providers/hashing.provider';
 
 @Module({
   controllers: [AuthController],
@@ -15,9 +14,10 @@ import { HashingProvider } from './providers/hashing.provider';
     SignInProvider,
     SignUpProvider,
     SocialProvider,
-    HassingProvider,
-    BcryptProvider,
-    HashingProvider,
+    {
+      provide: HashingProvider,
+      useClass: BcryptProvider,
+    },
   ],
 })
 export class AuthModule {}
