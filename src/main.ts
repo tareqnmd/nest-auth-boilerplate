63e3042b2852fcc4db6e6bffa743580faedc9115
Tareq <tareqnmd@gmail.com>
2025-09-13 21:44:55 +0600

    Add global ClassSerializerInterceptor to NestJS application: enhance response serialization for improved API output consistency.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import { ClassSerializerInterceptor } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(configService.get('appConfig.port') ?? 3201);
 }
