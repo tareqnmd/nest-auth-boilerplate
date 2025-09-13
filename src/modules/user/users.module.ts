fc11ccefd76ca2f336df24350854d65dd14dd7fc
Tareq <tareqnmd@gmail.com>
2025-09-13 15:53:47 +0600

    Integrate TypeORM with UsersModule: import TypeOrmModule for UserEntity and export UserService for improved user management and database interaction.

--- src/modules/user/users.module.ts
+++ src/modules/user/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GetUserByEmailProvider } from './providers/get-user-by-email.provider';
 import { GetUserByIdProvider } from './providers/get-user-by-id.provider';
 import { UserService } from './providers/user.service';
+import { UserEntity } from './user.entity';
 import { UserController } from './users.controller';
 
 @Module({
   controllers: [UserController],
   providers: [UserService, GetUserByEmailProvider, GetUserByIdProvider],
+  exports: [UserService],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
 })
 export class UsersModule {}
