26e6747476a28bd029b936c13361b9860d0891c8
Tareq <tareqnmd@gmail.com>
2025-09-13 12:48:41 +0600

    Integrate ConfigModule for environment configuration: add global configuration setup in app.module.ts, update main.ts to use ConfigService for dynamic port assignment, and enhance app.config.ts and env.validation.ts for improved environment variable handling.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfig from './config/app.config';
+import dbConfig from './config/db.config';
+import envValidation from './config/env.validation';
+import jwtConfig from './config/jwt.config';
+import socialConfig from './config/social.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/user/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+      load: [appConfig, dbConfig, jwtConfig, socialConfig],
+      validationSchema: envValidation,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('appConfig', () => ({
-  port: process.env.PORT,
+  port: parseInt(process.env.PORT ?? '3201', 10),
   env: process.env.NODE_ENV,
 }));
