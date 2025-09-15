7977399d41f0d553c51da1be085c3e0b1af533ee
Tareq <tareqnmd@gmail.com>
2025-09-16 00:41:46 +0600

    Add Google authentication support: integrate google-auth-library for token verification and enhance GoogleAuthProvider with improved authentication methods and error handling.

--- src/modules/auth/providers/google-auth.provider.ts
+++ src/modules/auth/providers/google-auth.provider.ts
@@ -1,19 +1,55 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  OnModuleInit,
+  UnauthorizedException,
+} from '@nestjs/common';
 import type { ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
 import socialConfig from 'src/config/social.config';
 
 @Injectable()
-export class GoogleAuthProvider {
+export class GoogleAuthProvider implements OnModuleInit {
+  private oAuthClient: OAuth2Client;
   constructor(
     @Inject(socialConfig.KEY)
     private readonly socialConfiguration: ConfigType<typeof socialConfig>,
   ) {}
 
-  googleAuth(token: string) {
-    return { token };
+  onModuleInit() {
+    this.oAuthClient = new OAuth2Client(
+      this.socialConfiguration.googleClientId,
+      this.socialConfiguration.googleClientSecret,
+    );
   }
 
-  authenticate(token: string) {
-    return { token };
+  async authenticate(token: string) {
+    try {
+      const ticket = await this.oAuthClient.verifyIdToken({
+        idToken: token,
+      });
+      const payload = ticket.getPayload();
+      return payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+  }
+
+  async googleAuth(token: string) {
+    try {
+      const data = await this.authenticate(token);
+      if (!data) {
+        throw new UnauthorizedException();
+      }
+      return {
+        firstName: data.given_name,
+        lastName: data.family_name,
+        email: data.email,
+        image: data.picture,
+        googleId: data.sub,
+      };
+    } catch {
+      throw new UnauthorizedException();
+    }
   }
 }
