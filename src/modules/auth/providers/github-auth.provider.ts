8c7a1482dea96ec23823c60451a25464b903f571
Tareq <tareqnmd@gmail.com>
2025-09-15 23:01:47 +0600

    Refactor GithubAuthProvider and GoogleAuthProvider constructors to inject social configuration: enhance dependency injection for improved configuration management.

--- src/modules/auth/providers/github-auth.provider.ts
+++ src/modules/auth/providers/github-auth.provider.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import type { ConfigType } from '@nestjs/config';
+import socialConfig from 'src/config/social.config';
 
 @Injectable()
 export class GithubAuthProvider {
-  constructor() {}
+  constructor(
+    @Inject(socialConfig.KEY)
+    private readonly socialConfiguration: ConfigType<typeof socialConfig>,
+  ) {}
 
   githubAuth(token: string) {
     return { token };
--- src/modules/auth/providers/google-auth.provider.ts
+++ src/modules/auth/providers/google-auth.provider.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import type { ConfigType } from '@nestjs/config';
+import socialConfig from 'src/config/social.config';
 
 @Injectable()
 export class GoogleAuthProvider {
-  constructor() {}
+  constructor(
+    @Inject(socialConfig.KEY)
+    private readonly socialConfiguration: ConfigType<typeof socialConfig>,
+  ) {}
 
   googleAuth(token: string) {
     return { token };
